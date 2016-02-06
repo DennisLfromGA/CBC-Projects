1fc33840f866f79d56ea15be8ff849eb8dd1de9b
DennisL <Denny.Lockhart@gmail.com>
2016-02-06 14:39:59 -0500

    Update current-vers.sh
    Added backup/save features and cleaned up a little.

--- current-vers.sh
+++ current-vers.sh
@@ -3,8 +3,8 @@
 folder="$HOME/Downloads/CBC/current-vers"
 
 # Output file name
-out=current-vers.csv
-omaha=omaha-vers.csv
+out=${folder}/current-vers.csv
+omaha=${folder}/omaha-vers.csv
 
 ## Local vers file for comparison
 local="${omaha}.sav"
@@ -12,7 +12,15 @@ local="${omaha}.sav"
 ## URL of omahaproxy
 url='https://cros-omahaproxy.appspot.com/all'
 
-cd $folder || exit 2
+if [ ! -d "$folder" ]; then
+  if ! mkdir -p "$folder"; then
+    echo "Folder '$folder' does not exist and cannot be created - aborting!"
+  else
+    cp $0 "$folder" 2>/dev/null
+  fi
+fi
+
+cd $folder
 
 ##
 echo "Downloading current Chrome OS versions..."
@@ -27,22 +35,23 @@ fi
 # Check to see if the omaha file has changed.
 if diff -q $omaha $local 2>/dev/null; then
   echo "The omaha versions have not changed."
-  echo "No need to proceed with generation of '$out'."
+  echo "No need to proceed with generation of '${out##*/}'."
   exit 0
 else
   echo "The omaha versions have changed!"
   cp $omaha $local 
-  echo "Continuing with generation of '$out' ..."
+  echo "Continuing with generation of '${out##*/}' ..."
   echo "Be sure to update the github site."
 fi
 
-mv $out /tmp 2>/dev/null
+mv $out ${out}.sav 2>/dev/null
 
 ## Generate csv file for import
 echo -n "Generating new, sorted csv file for import - "
-echo "Hardware,Track,Chrome Vers,ChromeOS Vers." > $out 
+echo "Hardware,Track,Chrome Vers.,ChromeOS Vers." > $out 
 grep -v timestamp < $omaha |\
   awk -F',' '{printf "%s,%s,%s,%s\n",$6,$5,$3,$2}' |\
+  sed 's/"/-inch/' |\
   sort -u >> $out 
 
 echo "finished."
